cc7a0743b2cae99e81d3456127382600a8e027b0
Prabin Dangol <neprabin@gmail.com>
2025-02-26 16:46:05 +0545

    Update super-product-filter.php

--- super-product-filter.php
+++ super-product-filter.php
@@ -11,7 +11,6 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       super-product-filter
  * Domain Path:       /languages
- * Requires Plugins:  woocommerce
  */
 // If this file is called directly, abort.
 if (!defined('WPINC')) {
